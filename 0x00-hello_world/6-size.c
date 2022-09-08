6134dc2445094b5efd6a1a4850821f41824c8cfc
CharlyAnne <icharlieanne@gmail.com>
2022-09-08 10:57:34 -0700

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+/*
+ * File: 6-size.c
+ * Auth: CharlyAnnne
+ */
+#include <stdio.h>
+/**
+ * main - print to screen
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+		printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	return (0);
+}
