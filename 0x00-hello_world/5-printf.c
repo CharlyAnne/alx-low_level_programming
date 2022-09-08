2e69ee8d6887781d301602ceadf4bb7abeecee69
CharlyAnne <icharlieanne@gmail.com>
2022-09-08 10:23:22 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/*
+ * File: 5-printf.c
+ * Auth: CharlyAnne
+ */
+#include <stdio.h>
+
+/**
+ * main - print to screen
+ *
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
