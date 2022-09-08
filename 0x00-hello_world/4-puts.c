b97c264f2565f8ab737534ae0cfd919fc67d1965
CharlyAnne <icharlieanne@gmail.com>
2022-09-08 07:37:57 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Zero (0) (Success)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
