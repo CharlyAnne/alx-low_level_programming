21012c14220bfe22e37e51a4fa5cd55cae48ff72
CharlyAnne <icharlieanne@gmail.com>
2022-09-08 09:29:58 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,12 +9,11 @@
  * main - Prints "Programming is like building a multilingual puzzle, 
  * 			followed by a new line.
  *
- * return: 0 always.
+ * return: Always 0.
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
-
