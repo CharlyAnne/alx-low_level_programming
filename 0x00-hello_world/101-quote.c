285bde8e839702efc19605d949da5ef6529a19f6
CharlyAnne <icharlieanne@gmail.com>
2022-09-08 12:25:44 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+/*
+ * File - 101-quote.c
+ * Auth: CharlyAnne
+ */
+#include <unistd.h>
+/**
+ * main - Print to screen
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
