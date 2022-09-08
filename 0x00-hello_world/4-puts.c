0ad97f6b5f068a55e4a455fdc00d940992fda4b3
CharlyAnne <icharlieanne@gmail.com>
2022-09-08 09:54:00 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,18 +2,15 @@
  * File: 4-puts.c
  * Auth: CharlyAnne
  */
-
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle, 
- * 			followed by a new line.
+ * main- Print to screen.
  *
- * return: Always 0.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
