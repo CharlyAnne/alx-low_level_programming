5fcdd125340f64b1de9b8e99bd68d37dd07388d9
CharlyAnne <icharlieanne@gmail.com>
2022-09-09 10:29:28 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'output base 10 numbers'
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+int count = 0;
+while (count < 10)
+printf("%d", count++);
+printf("\n");
+return (0);
+}
