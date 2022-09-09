fa2c989622080c18e3b9e90332bb39c7d0577696
CharlyAnne <icharlieanne@gmail.com>
2022-09-09 10:07:28 -0700

    fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@ char ch = 'a';
 char upper_ch = 'A';
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
-for (upper_ch = 'A'; upper_ch <= 'z'; upper_ch++)
+for (upper_ch = 'A'; upper_ch <= 'Z'; upper_ch++)
 putchar(upper_ch);
 putchar('\n');
 return (0);
