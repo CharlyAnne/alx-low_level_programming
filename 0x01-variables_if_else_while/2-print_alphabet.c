946a4b6c01641d5339737a86d2320e6151ee9e1c
CharlyAnne <icharlieanne@gmail.com>
2022-09-09 09:35:19 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,8 @@
 int main(void)
 {
 	char ch = 'a';
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-
-		putchar('\n');
-
-	return (0);
+for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	putchar('\n');
+return (0);
 }
