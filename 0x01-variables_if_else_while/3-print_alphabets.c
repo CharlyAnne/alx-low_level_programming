efea0e1b8145e90e140422b13fc89d57d5b9507f
CharlyAnne <icharlieanne@gmail.com>
2022-09-09 09:59:10 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.o>
+/**
+ * main - Entry point
+ * Description: output letters in small and capital letters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a';
+char upper_ch = 'A';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (upper_ch = 'A'; upper_ch <= 'z'; upper_ch++)
+putchar(upper_ch);
+putchar('\n');
+return (0);
+}
