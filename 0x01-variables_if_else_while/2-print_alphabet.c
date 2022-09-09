4d865953c90f2a1f95552595b2daa0f7ccab53c5
CharlyAnne <icharlieanne@gmail.com>
2022-09-09 09:45:14 -0700

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,13 @@
 /**
  * main - print alphabet in lowercase
  *
- * Return: zero (0)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch = 'a';
+char ch = 'a';
 for (ch = 'a'; ch <= 'z'; ch++)
-	putchar(ch);
-	putchar('\n');
+putchar(ch);
+putchar('\n');
 return (0);
 }
