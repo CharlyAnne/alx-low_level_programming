cef56a13fe69671e86f618b3cea6f4d22ef5d253
CharlyAnne <icharlieanne@gmail.com>
2022-09-13 09:16:05 -0700

    prt aplh

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints 10 times the alphabet in lowercase
- * Return : Always return 0
+ * print_alphabet_x10 - print 10 times the alphabet in lowercase
+ * Return : Always 0
 */
 void print_alphabet_x10(void)
 {
@@ -19,6 +19,6 @@ _putchar(ch);
 ch++;
 }
 _putchar('\n');
-i++
+i++;
 }
 }
