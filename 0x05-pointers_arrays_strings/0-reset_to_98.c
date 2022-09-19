f62f93bc1ca800745c2c17910e55cc4a04c6761c
CharlyAnne <icharlieanne@gmail.com>
2022-09-19 04:27:01 -0700

    first pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function to update the refernce of a pointer
+ * @n: pointer to be updated.
+ *
+ * Return: Always 0
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b)i;
+int _strlen(char *s);
+void _puts(char *str);
+
+#endif
