237adc84339b1020725853f7a7d255b30c90db0a
CharlyAnne <icharlieanne@gmail.com>
2022-09-22 10:31:16 -0700

    maybe

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
-
 /**
- * main - to add one line to code so that program prints
- * description - a program that prints a[2] = 98
- *
- * Return: Always 0
-*/ 
+ * main - to add one line to code
+ * Return: Always 0.
+*/
 int main(void)
 {
 int n;
-int a[5]
+int a[5];
 int *p;
 
 a[2] = 1024;
 p = &n;
-/**
- * write your line of code here.
+/*
+ * write your line of code here...
  * Remember:
  * - you are not allowed to use a
  * - you are not allowed to modify p
  * - only one statement
- * - you are not allowed to code anything else than this line of code.
+ * - you are not allowed to code anything else than this line of code
 */
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
