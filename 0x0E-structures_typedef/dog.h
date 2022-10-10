5c2f90ac654683733f0bdda1691dba43cf5f4253
CharlyAnne <icharlieanne@gmail.com>
2022-10-10 06:22:48 -0700

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,7 @@ struct dog
 	char *owner;
 };
 
-typeof struct dog dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
