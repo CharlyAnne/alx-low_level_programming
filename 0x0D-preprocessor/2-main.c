ac315997e60168f0d2f6c06af921869b60a7923d
CharlyAnne <icharlieanne@gmail.com>
2022-10-10 05:08:29 -0700

    Preprocessors

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints name of file
+ *
+ * Return: 0.
+*/
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef ABSO
+#define ABSO
+
+#define ABS(x) (((X) < (0)) ? (-(X)) : (X))
+
+#endif /* ABS(X) */
